0be64684738d88479b3c1d2d7db5991fa1c899f9
aayushi11personaluse-wq <aayushi11personaluse@gmail.com>
2026-07-02 19:52:06 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const startButton = document.getElementById("startBtn");
+
+startButton.addEventListener("click", function () {
+    alert("🌸 Welcome to My Wonyoung Era!\n\nLet's build healthy habits together!");
+});
